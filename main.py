0fbdfdf51b9c8e5f14f0e834630dcd0e1308931b
Predator <umarbekusmanovich@gmail.com>
2026-04-30 00:02:27 +0500

    add many fitures and etc.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-from app.routers import auth, dashboard, groups
+from app.routers import auth, dashboard, groups, assignments, school, users
 from db.db_ext import init_db
 
 limiter = Limiter(key_func=get_remote_address)
